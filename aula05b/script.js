f443fdcbc38b9ef28925b70ed721481686c92160
ProfessorGarra <professorgarra@gmail.com>
2021-05-17 20:31:12 -0300

    aula05b

---
+++ aula05b/script.js
@@ -0,0 +1,5 @@
+
+        let nome=prompt('qual é seu nome')
+        document.write(`Olá, <strong>${nome}</strong>! Seu nome tem ${nome.length} letras.<br>`)
+        document.write(`Seu nome em letras maiúsculas ${nome.toUpperCase()} <br>`)
+        document.write(`Seu nome em letras minúscula é ${nome.toLowerCase()}`)
