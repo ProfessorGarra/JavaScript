03a06ad67239adb9cdd69fff49022d388e1ac70b
ProfessorGarra <professorgarra@gmail.com>
2021-05-25 04:04:20 -0300

    aula06

--- aula06/script.js
+++ aula06/script.js
@@ -3,3 +3,7 @@ let saldoHoras=0;
 let horasTrabalhadas=prompt('Informe o total de horas trabalhadas do funcionário nessa semana:','Digite aqui');
 saldoHoras=horasTrabalhadas-cargaHorariaSemanal;
 if(saldoHoras>0){alert('O funcionário fez '+saldoHoras+' hora(s) extra(s) nesta semana');}else if(saldoHoras<0){alert('O funcionário está devendo'+(saldoHoras*-1)+'hora(s) nesta semana');}else{alert('O funcionário não possui hora extra essa semana');}
+
+let idade=prompt('informe sua idade','digite aqui');
+let resposta= idade >=18?'Maior':'Menor';
+alert(resposta);
