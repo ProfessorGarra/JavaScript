5b8b5569751295d1511d45351c991afb5ff67c83
ProfessorGarra <professorgarra@gmail.com>
2021-06-22 03:33:04 -0300

    aula11

---
+++ aula11/script.js
@@ -0,0 +1,12 @@
+let number=prompt('Digite um número entre 1 e 3','Digite aqui');
+if(number==1){
+    document.write('Número 01')
+}
+else if(number==2){
+    document.write('Número 02')
+}
+else if(number==3){
+    document.write('Número 03')
+}else{
+    document.write('Sabe ler não? Abestado!')
+}
